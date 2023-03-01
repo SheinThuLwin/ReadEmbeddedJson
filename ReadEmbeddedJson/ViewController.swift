a18b6e7f694aa39541df018b04942039f1b2b4f3
Shein Thu Lwin <sheinthu.lwin@wavemoney.com.mm>
2023-03-01 13:57:24 +0630

    Add json file to read

--- ReadEmbeddedJson/ViewController.swift
+++ ReadEmbeddedJson/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
+struct Person : Decodable {
+    var name: String
+    var age: String
+    var employed: String
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .gray
+        
+        if let pserson = readJsonModel(filename: "MyJson") {
+            print("Person >> \(pserson.name), \(pserson.age), \(pserson.employed)")
+        }
+        
+        if let dic = readJsonDic(filename: "MyJson") {
+            print("Person(dictionary) >> \(dic["name"]), \(dic["age"]), \(dic["employed"])")
+        }
     }
-
-
+    
+    func readJsonModel(filename fileName: String) -> Person? {
+        if let url = Bundle.main.url(forResource: fileName, withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode(Person.self, from: data)
+                return jsonData
+            } catch {
+                print("error:\(error)")
+            }
+        }
+        return nil
+    }
+    
+    func readJsonDic(filename fileName: String) -> [String: AnyObject]? {
+        if let url = Bundle.main.url(forResource: fileName, withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let object = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                if let dictionary = object as? [String: AnyObject] {
+                    return dictionary
+                }
+            } catch {
+                print("error:\(error)")
+            }
+        }
+        return nil
+    }
 }
 
